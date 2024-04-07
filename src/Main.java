9ef1cbe3202f364f1f39542176b990e326cfa909
Daulet <kantaidaulet@gmail.com>
2024-04-08 00:22:19 +0500

    Commit 3

--- src/Main.java
+++ src/Main.java
@@ -13,9 +13,22 @@ public class Main {
         int task_number = scanner.nextInt();
 
         if(task_number == 1){
+            System.out.print("Num of elements");
             int n = scanner.nextInt();
             ArrayList<Integer> arr = new ArrayList<>();
             System.out.println(Methods.problem1_find_minimum(n,arr,scanner));
         }
+        else if(task_number == 2){
+            ArrayList<Integer> arr = new ArrayList<>();
+            System.out.print("Num of elements:");
+            int n = scanner.nextInt();
+            System.out.println(Methods.problem2_average(n,arr,scanner));
+        } else if (task_number == 3) {
+            int n = scanner.nextInt();
+            System.out.print("Enter a num:");
+            if(Methods.problem3_prime_number(n)) System.out.println("Prime");
+            else System.out.println("Coposite");
+        }
+
     }
 }
--- src/Methods.java
+++ src/Methods.java
@@ -14,4 +14,27 @@ public class Methods {
             return problem1_find_minimum(n-1,arr,scanner);
         }
     }
+    public static double problem2_average(int n,ArrayList<Integer> arr,Scanner scanner){
+        if(n<=0){
+            double sum = 0 ;
+            for(int i:arr){
+                sum+=i;
+            }
+            return  sum/arr.size();
+        }
+        else{
+            int temp = scanner.nextInt();
+            arr.add(temp);
+            return problem2_average(n-1,arr,scanner);
+        }
+    }
+
+    public static boolean problem3_prime_number(int n){
+        short count = 0;
+        for(int i=2;i<=n;i++){
+            if(n%i==0) count++;
+        }
+        if(count==1) return true;
+        else return false;
+    }
 }
